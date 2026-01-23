c78740e1ce023a925f5a33b468c1c4606d1e9801
Ali <ahmedlali11ll67@gmail.com>
2026-01-22 23:00:09 -0800

    Version 2

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 console.log('Config.js')
 console.log('Version 1')
+console.log('This verion is also updated')
+//This version will be deleted once it has been read
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 console.log('Index.js')
 console.log('Version 1')
+console.log('Updated version of the index js')
+const a = 0;
+console.log(a)
